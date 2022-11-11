b947dbf1ad7b5f8ef8f3b167d3153a988611ebf0
Pranav <pranav.techiegeek@gmail.com>
2022-11-11 15:34:10 +0530

    basic boilerplate ready

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+
+API Routes
+
+Discover 
+https://api.themoviedb.org/3/discover/movie?sort_by=opularity.desc&api_key=
+
+Search 
+https://api.themoviedb.org/3/search/movie?api_key=""&query=""
+
+*/
+
+const main = document.getElementById("main");
+const form = document.getElementById("search_form");
+const search = document.getElementById("search");
+const logo = document.querySelector(".logo");
